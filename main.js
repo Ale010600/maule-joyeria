d2d07e7642db599e480e6cf61ed75b80a5ace9d3
Ale010600 <161529081+Ale010600@users.noreply.github.com>
2024-02-28 19:06:02 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.carrusel');
+ScrollReveal().reveal('.contenido', { delay: 500 });
+ScrollReveal().reveal('.piebanner', { delay: 500 });
+ScrollReveal().reveal('.social', { delay: 500 });
